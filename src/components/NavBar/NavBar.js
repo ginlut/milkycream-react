f9d2696b33839a43986d44db904a95582368b258
ginlut <ginalutfallah@gmail.com>
2022-03-27 18:53:58 -0300

    Tarea Clase 2

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import './NavBar.css'
+
+const NavBar = () => {
+
+    return ( 
+        <nav className = "NavBar" >
+            <div>
+                <h3> Milkycream </h3> 
+            </div> 
+            <button>Milkshakes</button>
+            <button>Galletas</button>
+            <button>Waffles </button>
+        </nav>
+
+    )
+}
+
+export default NavBar
